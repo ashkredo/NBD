9b9209815c39ccb7d3868f6a32ef7a9b741372f0
Artur Shkred <s15444@pjwstk.edu.pl>
2020-12-13 16:20:07 +0100

    Update zapytanie_4_A.js

--- Cwiczenie4/zapytanie_4_A.js
+++ Cwiczenie4/zapytanie_4_A.js
@@ -2,13 +2,13 @@
 printjson(db.people.aggregate([
   {$addFields: {
       bmi : {$divide : [{$toDouble:"$weight"}, {$pow : [{$divide : [{$toDouble:"$height"}, 100]}, 2]}]}
-		}
-	},
-	{$group : {
-		 _id : "$nationality",
-		 avgBMI : {$avg : "$bmi"},
-		 minBMI : {$min : "$bmi"},
-		 maxBMI : {$max : "$bmi"}
-	 }
-	}
+    }
+  },
+  {$group : {
+      _id : "$nationality",
+      avgBMI : {$avg : "$bmi"},
+      minBMI : {$min : "$bmi"},
+      maxBMI : {$max : "$bmi"}
+    }
+  }
 ]).toArray());
