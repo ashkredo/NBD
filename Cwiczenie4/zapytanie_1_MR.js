ebc6e52523f02997915f23137789bf566045fdcd
Artur Shkred <s15444@pjwstk.edu.pl>
2020-12-13 16:16:08 +0100

    Update zapytanie_1_MR.js

--- Cwiczenie4/zapytanie_1_MR.js
+++ Cwiczenie4/zapytanie_1_MR.js
@@ -8,17 +8,17 @@ printjson(db.people.mapReduce(
   	for (var idx = 0; idx < values.length; idx++) {
   		reducedVal.count += values[idx].count;
   		reducedVal.weight += values[idx].weight;
-      reducedVal.height += values[idx].height;
-  	}
-      return reducedVal;
-  },
-	{
-		out: "avg_weight_and_heigth",
-		finalize: function (key, reducedValue) {
-    	return {
-        avgWeight : reducedValue.weight / reducedValue.count,
-        avgHeight : reducedValue.height / reducedValue.count
-      };
-    }
+		reducedVal.height += values[idx].height;
 	}
+	return reducedVal;
+  },
+  {
+	out: "avg_weight_and_heigth",
+	finalize: function (key, reducedValue) {
+		return {
+			avgWeight : reducedValue.weight / reducedValue.count,
+			avgHeight : reducedValue.height / reducedValue.count
+		};
+	}
+  }
 ).find().toArray());
