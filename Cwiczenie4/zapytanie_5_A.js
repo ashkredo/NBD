bc666aa38c094eff4eb5a9cadcc9b88cb420cbf5
Artur Shkred <s15444@pjwstk.edu.pl>
2020-12-13 16:31:30 +0100

    Update zapytanie_5_A.js

--- Cwiczenie4/zapytanie_5_A.js
+++ Cwiczenie4/zapytanie_5_A.js
@@ -1,14 +1,15 @@
 // -- Średnia i łączna ilość środków na kartach kredytowych kobiet narodowości polskiej w podziale na waluty
 printjson(db.people.aggregate([
-	{$match : {
-    nationality : "Poland",
-    sex : "Female"}
+  {$match : {
+      nationality : "Poland",
+      sex : "Female"
+    }
   },
   {$unwind : "$credit"},
-	{$group : {
-			_id : "$credit.currency",
+  {$group : {
+      _id : "$credit.currency",
       "avgBalance" : {$avg : {$toDouble : "$credit.balance"}},
-			"totalBalance" : {$sum : {$toDouble : "$credit.balance"}}
-		}
-	}
+      "totalBalance" : {$sum : {$toDouble : "$credit.balance"}}
+    }
+  }
 ]).toArray());
